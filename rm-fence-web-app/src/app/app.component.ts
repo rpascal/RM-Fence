5e6612f4351008f56370e43569454c21dd3b7d72
Ryan Pascal <ryan.david.pascal@gmail.com>
2022-05-22 10:18:32 -0400

    Simple routing / module setup

--- rm-fence-web-app/src/app/app.component.ts
+++ rm-fence-web-app/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rmf-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-  title = 'rm-fence-web-app';
+
 }
