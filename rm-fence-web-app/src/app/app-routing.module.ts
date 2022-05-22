6b34e3940fa9854213d094bbd9800f902676adef
Ryan Pascal <ryan.david.pascal@gmail.com>
2022-05-22 18:50:23 -0400

    Eager load

--- rm-fence-web-app/src/app/app-routing.module.ts
+++ rm-fence-web-app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule)
+    component: HomeComponent
+    // loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule)
   },
   {
     path: "**",
